de2cd42ab2d4c1f71fd50436585d501c735a9f62
Deepanshu Singh <anshu3012ep@gmail.com>
2026-05-04 15:07:33 +0530

    [ADD] game dto for data GET request endpoints

--- Program.cs
+++ Program.cs
@@ -1,6 +1,31 @@
+using GameStore.Api.DTOS;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+List<GameDto> games = [
+    new(1,
+        "Street Fighter",
+        "Fighting game",
+        19.99M,
+        new DateOnly(1992,7,15)
+    ),
+    new(2,
+        "Street Fighter 2",
+        "Fighting game",
+        25M,
+        new DateOnly(1993,7,15)
+    )
+];
+
+//GET endpoint /games
+app.MapGet("/games", () => games);
+
+// GET endpoint /games/{id}
+app.MapGet("/games/{id}", (int id) => games.Find(g => g.Id == id));
+
+//POST endpoint /games
+
 
 app.Run();
