ebc86167d999b2e2b69d306388ed69cf9966daa2
Deepanshu Singh <anshu3012ep@gmail.com>
2026-05-04 21:51:02 +0530

    ef core package to run and add database

--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
+using GameStore.Api.Data;
 using GameStore.Api.Endpoints;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddValidation();
+
+var connectionString = "Data Source=gamestore.db";
+builder.Services.AddSqlite<GameStoreContext>(connectionString);
 
 var app = builder.Build();
 app.MapGameEndpoints();
